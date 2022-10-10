c8681530cafaba8bdd43546e476601a64b136e5d
amar673 <102217540+amar673@users.noreply.github.com>
2022-10-11 03:08:09 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     int a,b,i;     cin>>a>>b;     for (i=1;i<=b;i=i+2)     {         cout<<a<<" x "<<i<<" = "<<a*i;         cout<<" ";     } }

---
+++ Tables.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    for (i=1;i<=b;i=i+2)
+    {
+        cout<<a<<" x "<<i<<" = "<<a*i;
+        cout<<"
+";
+    }
+}
