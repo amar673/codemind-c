bf72d85972aba01319a3683843664c12b691b5ec
amar673 <102217540+amar673@users.noreply.github.com>
2022-10-10 14:53:31 +0530

    #include<bits/stdc++.h> using namespace std; int main() {     float a,c,f;     cin>>a;     f=5.0/9.0;     //cout<<f;       c=a-32;     cout<<fixed << setprecision(2)<<f*c; }

---
+++ Fahrenheit_to_Celsius.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,c,f;
+    cin>>a;
+    f=5.0/9.0;
+    //cout<<f;
+      c=a-32;
+    cout<<fixed << setprecision(2)<<f*c;
+}
