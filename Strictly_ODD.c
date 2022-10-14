10ff887aa78fccba13878cbd363942c040855ed8
amar673 <102217540+amar673@users.noreply.github.com>
2022-10-14 14:58:48 +0000

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     cin>>n;     int a[n];     for(int i=0;i<n;i++) cin>>a[i];     int c=0;     for (int i=0;i<n;i++){         if (i%2==0){             if (a[i]%2) {                 c=1;                 break;             }             }         }     if (c==1) cout<<"False";     else cout<<"True"; }

---
+++ Strictly_ODD.c
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    int c=0;
+    for (int i=0;i<n;i++){
+        if (i%2==0){
+            if (a[i]%2) {
+                c=1;
+                break;
+            }
+            }
+        }
+    if (c==1) cout<<"False";
+    else cout<<"True";
+}
